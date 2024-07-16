81934bdd527c2a735e8d5b426a3f07adf4d58fe2
Usman Liaqat <memerboyemiology444@gmail.com>
2024-07-16 21:45:33 +0500

    chore: Remove unused files and dependencies

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: "outfit",
+          fontSize: 24,
+        }}
+      >
+        Hy By Why?
+      </Text>
+    </View>
+  );
+}
