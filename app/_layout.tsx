cf228329dfd00e0931a37ac0873850a5812543ce
Usman Liaqat <memerboyemiology444@gmail.com>
2024-07-17 12:12:15 +0500

    implementing get started page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,14 +3,19 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   useFonts({
-    "outfit": require("@/assets/fonts/Outfit-Regular.ttf"),
+    outfit: require("@/assets/fonts/Outfit-Regular.ttf"),
     "outfit-medium": require("@/assets/fonts/Outfit-Medium.ttf"),
     "outfit-bold": require("@/assets/fonts/Outfit-Bold.ttf"),
   });
 
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 }
